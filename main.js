88954342c2ca30941ea02c5413c10f39609f0969
Serhii Babych <50763978+SerhiiBabych@users.noreply.github.com>
2025-04-16 16:29:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.getElementById("nav");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    hamburger.classList.toggle("active");
+  });
+
 
